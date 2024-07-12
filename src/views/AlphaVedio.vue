2fe9c63b3c2fb5137c91fd3937be2ed42bac2c86
atian <atian@micous.com>
2024-07-12 12:05:12 +0800

    feat:透明通道修改

--- src/views/AlphaVedio.vue
+++ src/views/AlphaVedio.vue
@@ -1,18 +1,17 @@
+<!-- 1376 × 1068 -->
 <template>
   <div class="about">
     <video
       ref="video"
+      class="video"
       src="../assets/video/battle.mp4"
       autoplay="true"
-      width="420"
-      height="300"
       controls
       loop
       muted
     ></video>
-    <hr />
-    <canvas ref="canvas1" width="1500" height="1064" style="width: 420px; height: 300px"></canvas>
-    <canvas ref="canvas2" width="1500" height="1064" style="width: 420px; height: 300px"></canvas>
+    <canvas ref="canvas1" width="1376" height="1068" style="width: 15rem"></canvas>
+    <canvas class="canvas2" ref="canvas2" width="1376" height="1068"></canvas>
   </div>
 </template>
 
@@ -33,29 +32,36 @@ export default {
     const c2 = this.$refs.canvas2
     this.ctx2 = c2.getContext('2d', { willReadFrequently: true })
     const video = this.$refs.video
+    video.addEventListener('canplay', function () {
+      this.width = this.videoWidth
+      this.height = this.videoHeight
+    })
     video.addEventListener(
       'play',
       () => {
         let $this = this
+        const w = video.width
+        const h = video.height
+        console.log(w, h)
         ;(function loop() {
           if (!video.paused && !video.ended) {
             $this.ctx2.drawImage(video, 0, 0)
-            let frameData = $this.ctx2.getImageData(0, 0, 1500, 1064)
+            let frameData = $this.ctx2.getImageData(0, 0, w, h)
             let data = frameData.data
-            for (let i = 0; i < 1064; i++) {
-              for (let j = 0; j < 1500; j++) {
-                if (j < 750) {
+            for (let i = 0; i < h; i++) {
+              for (let j = 0; j < w; j++) {
+                if (j < w / 2) {
                   if (
-                    data[i * 1500 * 4 + j * 4] === 0 &&
-                    data[i * 1500 * 4 + j * 4 + 1] === 0 &&
-                    data[i * 1500 * 4 + j * 4 + 2] === 0
+                    data[i * w * 4 + j * 4] < 200 &&
+                    data[i * w * 4 + j * 4 + 1] < 200 &&
+                    data[i * w * 4 + j * 4 + 2] < 200
                   ) {
-                    data[i * 1500 * 4 + (j + 750) * 4 + 3] = 0
+                    data[i * w * 4 + (j + w / 2) * 4 + 3] = 0
                   }
                 }
               }
             }
-            $this.ctx1.putImageData(frameData, -750, 0, 750, 0, 750, 1064)
+            $this.ctx1.putImageData(frameData, -w / 4, 0, w / 2, 0, w / 2, h)
             setTimeout(loop, 1000 / 24)
           }
         })()
@@ -68,6 +74,19 @@ export default {
 
 <style lang="scss" scoped>
 .about {
+  padding-top: 15rem;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  //   background-color: red;
   //   padding: 2rem;
+  .video {
+    position: absolute;
+    z-index: -1;
+    opacity: 0;
+  }
+  .canvas2 {
+    opacity: 0;
+  }
 }
 </style>
